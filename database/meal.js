035c3a369b8a08290a392c97097b15e54ce0826f
shubhangijshinde <shubhangishinde1089@gmail.com>
2024-10-20 15:40:32 -0700

    fetching data and sharing meal

--- database/meal.js
+++ database/meal.js
@@ -1,6 +1,9 @@
+import fs from 'node:fs';
 import sql from 'better-sqlite3';
 
 const db = sql('meals.db');
+import slugify from 'slugify';
+import xss from 'xss';
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -10,4 +13,37 @@ export async function getMeals() {
 
 export function getMeal(slug) {
   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
+
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split('.').pop();
+  const fileName = `${meal.slug}.${extension}`;
+
+  const stream = fs.createWriteStream(`assets/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error('Saving image failed!');
+    }
+  });
+
+  meal.image = `/assets/${fileName}`;
+
+  db.prepare(`
+    INSERT INTO meals
+      (title, summary, instructions, creator, creator_email, image, slug)
+    VALUES (
+      @title,
+      @summary,
+      @instructions,
+      @creator,
+      @creator_email,
+      @image,
+      @slug
+    )
+  `).run(meal);
 }
